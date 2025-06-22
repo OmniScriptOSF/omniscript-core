08b89b8fcc1d08593feb8bc2fcd3e2e28ab0af57
Alphin Tom <alpha912@users.noreply.github.com>
2025-06-22 11:27:55 +0200

    Remove unused lexer

--- parser/src/lexer.ts
+++
@@ -1,9 +0,0 @@
-export interface Token {
-  type: string;
-  value: string;
-}
-
-export function tokenize(input: string): Token[] {
-  const lines = input.split(/\r?\n/);
-  return lines.map(line => ({ type: 'line', value: line }));
-}
